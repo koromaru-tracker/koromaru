eec6f9010e8833d5d6077d209798d82e2d72b9e9
Alex Wellnitz <moin@wellnitz-alex.de>
2024-01-14 20:34:53 +0100

    Add HTML template engine and configure static files and routes

--- index/server/webserver.go
+++ index/server/webserver.go
@@ -4,14 +4,22 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
 	"github.com/koromaru-tracker/koromaru/index"
 	"github.com/koromaru-tracker/koromaru/index/api"
+	"github.com/koromaru-tracker/koromaru/index/frontend"
 	"github.com/koromaru-tracker/koromaru/index/types"
 	"gorm.io/gorm"
 )
 
 func Serve(cfg *types.Config, db *gorm.DB) {
-	app := fiber.New()
+	// Initialize standard Go html template engine
+	engine := html.New("./views", ".html")
+	app := fiber.New(fiber.Config{
+		Views: engine,
+		// Views Layout is the global layout for all template render until override on Render function.
+		ViewsLayout: "layouts/main",
+	})
 
 	// Set Database Connection to Fiber Context
 	app.Use(func(c *fiber.Ctx) error {
@@ -20,8 +28,13 @@ func Serve(cfg *types.Config, db *gorm.DB) {
 		return c.Next()
 	})
 
+	app.Static("/", "./dist")
+
+	// Configure Fiber App
 	app = Configure(app)
+
 	app = api.RegisterRoutes(app)
+	app = frontend.RegisterRoutes(app)
 
 	log.Fatal(app.Listen(":" + cfg.Webserver.Port))
 }
