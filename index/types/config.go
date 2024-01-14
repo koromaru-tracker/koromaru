cae37baae8fe115be4526da5e9fadcfb0ff5b7b2
Alex Wellnitz <moin@wellnitz-alex.de>
2024-01-14 20:35:18 +0100

    Add LoginRequest and LoginResponse types

--- index/types/config.go
+++ index/types/config.go
@@ -1,8 +1,9 @@
 package types
 
 type Config struct {
-	Name     string `yaml:"name"`
-	Database struct {
+	Name      string `yaml:"name"`
+	JWTSecret string `yaml:"jwt_secret"`
+	Database  struct {
 		Provider string `yaml:"provider"`
 		Path     string `yaml:"path"`
 	} `yaml:"database"`
---
+++ index/types/login.go
@@ -0,0 +1,10 @@
+package types
+
+type LoginRequest struct {
+	Username string `form:"username" json:"username" binding:"required"`
+	Password string `form:"password" json:"password" binding:"required"`
+}
+
+type LoginResponse struct {
+	Token string `json:"token"`
+}
