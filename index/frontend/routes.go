df4b8790486b43763d57b68e3d86895e70ef032f
Alex Wellnitz <moin@wellnitz-alex.de>
2024-01-14 20:34:34 +0100

    Add frontend routes for index and backend pages

---
+++ index/frontend/routes.go
@@ -0,0 +1,31 @@
+package frontend
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/koromaru-tracker/koromaru/index/frontend/backend"
+	"github.com/koromaru-tracker/koromaru/index/frontend/page"
+	"github.com/koromaru-tracker/koromaru/index/middleware"
+)
+
+func RegisterRoutes(app *fiber.App) *fiber.App {
+	app.Get("/", page.Index)
+	app.Get("/login", page.Login)
+	app.Post("/login", page.LoginRequest)
+
+	jwt := middleware.NewAuthMiddleware("test")
+
+	backendGroup := app.Group("/backend")
+	backendGroup.Get("/", jwt, backend.Index)
+
+	torrentGroup := backendGroup.Group("/torrent")
+	torrentGroup.Get("/list", jwt, backend.TorrentList)
+	torrentGroup.Get("/create", jwt, backend.TorrentCreateView)
+	torrentGroup.Post("/create", jwt, backend.TorrentCreate)
+	torrentGroup.Get("/view/:id", jwt, backend.TorrentView)
+
+	userGroup := backendGroup.Group("/user")
+	userGroup.Get("/list", jwt, backend.UserList)
+	userGroup.Get("/logout", jwt, backend.UserLogout)
+
+	return app
+}
